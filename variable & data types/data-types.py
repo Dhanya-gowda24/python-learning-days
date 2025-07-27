f1fc7fd82f7a77ca7b71b9bafd075c0777da50bc
Dhanya-gowda24 <dhanyathapl129@gmail.com>
2025-07-27 22:54:58 +0530

    Add files via upload

---
+++ variable & data types/data-types.py
@@ -0,0 +1,15 @@
+# data types
+
+name = "Dhanyatha"     # string
+age = 18               # int
+weight = 33.5          # float
+is_student = True      # bool
+
+print(type(age))       # type checking 
+
+# conversion
+weight_int = int(weight)
+
+x = "24"
+print(int(x)+age)
+
---
+++ variable & data types/variables.py
@@ -0,0 +1,13 @@
+# assign variables
+# variables name can contain letters(a-z , A-Z),numbers(0-9) and underscore(_)
+# variables name must contain letters or underscore
+# variables names are case-sensitive
+
+a=10
+b=34.76
+
+x,y,z= 12,24,76
+
+A=B=C=10
+
+is_switch_on=True
