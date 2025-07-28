e717c0054b9ef71fa384e2310a750b1e0530621a
Dhanya-gowda24 <dhanyathapl129@gmail.com>
2025-07-28 22:04:33 +0530

    Add files via upload

---
+++ Day 02/arithmatic-operator.py
@@ -0,0 +1,23 @@
+#arithmatic operations
+
+a = 24
+b = 74
+
+print(a + b)    # addition
+print(a - b)    # subtractions
+print(a * b)    # multiplication
+print(a / b)    # division
+print(a // b)   # floor division
+print(a % b)    # modulas | remainder
+print(a ** b)   # exponentiation
+
+print(a+b-b*2/4//3)
+
+# swap two variables
+
+a = a+b
+b = a-b
+a = a-b
+
+print("a:",a)
+print("b:",b)
---
+++ Day 02/input-output.py
@@ -0,0 +1,9 @@
+# input and output/print
+
+age = (input("age: "))    # input
+print(age)                # output
+
+boy_name = input("boy name: ")
+girl_name = input("girl name: ")
+
+print(f"{boy_name} loves {girl_name}")
